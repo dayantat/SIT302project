e6c9e7a42008a5e9c81b54648097016c508f5b10
Dayan Tatlock <dane@Dayans-MacBook-Pro.local>
2018-12-13 17:25:42 +1100

    Started On Quiz

--- django-env/SIT302/game/models.py
+++ django-env/SIT302/game/models.py
@@ -1,3 +1,23 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Question(models.Model):
+    question = models.CharField(max_length=200)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey("Question", related_name="choices")
+    choice = models.CharField("Choice", max_length=50)
+    position = models.IntegerField("position")
+
+    class Meta:
+        unique_together = [
+            # No Duplicated Choice per question
+            ("question", "choice"),
+            # No Duplicated position per question
+            ("question", "position")
+        ]
+        ordering = ("position",)
