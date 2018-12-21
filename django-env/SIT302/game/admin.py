29d57da700c11291d92bd6cb4c4d5f84317e2f14
Dayan Tatlock <dane@Dayans-MacBook-Pro.local>
2018-12-21 17:07:59 +1100

    answering done basic

--- django-env/SIT302/game/admin.py
+++ django-env/SIT302/game/admin.py
@@ -4,9 +4,20 @@ from .models import Question, Choice
 # Register your models here.
 
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 4
+
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question_text']
+    fieldsets = [
+        ("Question", {'fields': ['question_text']}),
+        ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
+
 
