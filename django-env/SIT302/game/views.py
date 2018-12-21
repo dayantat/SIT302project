bbc5055fae6c5f27e2fd0604a2eb2d7e3df53d45
Dayan Tatlock <dane@Dayans-MacBook-Pro.local>
2018-12-21 16:48:50 +1100

    Answering the Question

--- django-env/SIT302/game/views.py
+++ django-env/SIT302/game/views.py
@@ -1,28 +1,34 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-from django.http import Http404
-from django.views import generic
 from django.urls import reverse
-# Create your views here.
-from django.http import HttpResponse, HttpResponseRedirect
-from .models import Question
-from .models import Choice
+from django.views import generic
+from .models import Question, Choice
+from django.utils import timezone
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+class IndexView(generic.ListView):
+
     template_name = 'game/index.html'
-    context = {'latest_question_list': latest_question_list}
-    return render(request, template_name, context)
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'game/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'game/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
-def results(request, question_id):
+class ResultsView(generic.DetailView):
+    model = Question
     template_name = 'game/results.html'
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, template_name, {'question': question})
 
 
 def vote(request, question_id):
