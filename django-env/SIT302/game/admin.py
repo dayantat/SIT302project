72ec8b40d5ee64e8243c337dcf6897a2bf730086
Dayan Tatlock <dane@Dayans-MacBook-Pro.local>
2019-01-22 16:54:56 +1100

    Tengs page

--- django-env/SIT302/game/admin.py
+++ django-env/SIT302/game/admin.py
@@ -8,15 +8,18 @@ class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 4
 
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         ("Question", {'fields': ['question_text']}),
         ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+        ("Category", {'fields': ['category_text']}),
     ]
     inlines = [ChoiceInline]
     list_display = ('question_text', 'pub_date', 'was_published_recently')
     list_filter = ['pub_date']
     search_fields = ['question_text']
+
 
 admin.site.register(Question, QuestionAdmin)
 
--- django-env/SIT302/game/models.py
+++ django-env/SIT302/game/models.py
@@ -8,9 +8,19 @@ from django.utils import timezone
 # Each model has a number of class variables, each of which represents a database field in the model.
 # Each field is represented by an instance of a Field class – e.g., CharField for character fields and DateTimeField for datetimes. This tells Django what type of data each field holds.
 
+
+
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("Date Published")
+    EMAIL = "EMAIL"
+    review_field = (
+        (EMAIL, "Email"),
+        ("SPAM", "Spam"),
+        ("PHYSICAL", "Physical")
+    )
+    category_text = models.CharField(max_length=40, choices=review_field, default=EMAIL)
 
     def was_published_recently(self):
         now = timezone.now()
@@ -22,6 +32,22 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+
+
+#
+# class Category(models.Model):
+#     EMAIL = "EMAIL"
+#     review_field = (
+#         (EMAIL, "Email"),
+#         ("SPAM", "Spam"),
+#         ("PHYSICAL", "Physical")
+#     )
+#     category_text = models.CharField(max_length=40, choices=review_field, default=EMAIL)
+#
+#     question_text = models.ForeignKey(Question, on_delete=models.CASCADE)
+#
+#     def __str__(self):
+#         return self.category_text
 
 
 class Choice(models.Model):
